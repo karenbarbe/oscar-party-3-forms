c946f990f64778a8ab4750538d844eb2280eec85
Karen Barbé <info@karenbarbe.com>
2025-08-31 11:48:53 -0500

    add http basic auth

--- app/controllers/backdoor_controller.rb
+++ app/controllers/backdoor_controller.rb
@@ -1,4 +1,5 @@
 class BackdoorController < ApplicationController
+  http_basic_authenticate_with name: ENV.fetch("ADMIN_USERNAME"), password: ENV.fetch("ADMIN_PASSWORD")
   def backdoor_index
     render({ :template => "backdoor_templates/backdoor_index" })
   end
