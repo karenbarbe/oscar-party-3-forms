d03820c28d272a1d3918a72f318e6bba3d1b652c
Karen Barbé <info@karenbarbe.com>
2025-08-31 13:55:30 -0500

    Update backdoor_controller.rb

--- app/controllers/backdoor_controller.rb
+++ app/controllers/backdoor_controller.rb
@@ -5,10 +5,19 @@ class BackdoorController < ApplicationController
     render({ :template => "backdoor_templates/backdoor_index" })
   end
 
+  def directors_index
+    @list_of_directors = Director.all
+
+    render({ :template => "backdoor_templates/directors_index" })
+  end
+
   def create_director
     the_director = Director.new
     the_director.first_name = params.fetch("query_first_name")
     the_director.last_name = params.fetch("query_last_name")
+    the_director.dob = params.fetch("query_dob")
+    the_director.bio = params.fetch("query_bio")
+    the_director.image = params.fetch("query_image")
 
     if the_director.valid?
       the_director.save
@@ -16,6 +25,14 @@ class BackdoorController < ApplicationController
     else
       redirect_to("/backdoor", { :alert => the_director.errors.full_messages.to_sentence })
     end
+  end
 
+  def destroy_director
+    the_id = params.fetch("path_id")
+    the_director = Director.where({ :id => the_id }).first
+
+    the_director.destroy
+
+    redirect_to("/backdoor/directors", { :notice => "Director deleted successfully." })
   end
 end
